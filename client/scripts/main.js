4ef083b1c55ede9ce7a0e3657538dd1595fe18a8
milimetric <dandreescu@wikimedia.org>
2012-11-06 16:28:26 -0500

    Got chart element hierarchy stubs to load

--- client/scripts/main.js
+++
@@ -1,8 +0,0 @@
-infuser.defaults.templateUrl = "templates";
-require(['models/pages/Home'], function(Home){
-  return $(document).ready(function(){
-    var viewModel;
-    viewModel = new Home();
-    return ko.applyBindings(viewModel);
-  });
-});
